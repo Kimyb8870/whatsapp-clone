0cccead6b66b496813c544b2781ab40e5710f772
kimyb8870 <qhd0084@gmail.com>
2020-12-20 21:08:21 +0900

    Add: basic frame

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import "./App.css";
+import Sidebar from "./Sidebar";
+import Chat from "./Chat";
 
 function App() {
   return (
+    //BEM naming convention
     <div className="app">
-      <h1>hi</h1>
-      <h2>bye</h2>
+      <div className="app__body">
+        <Sidebar />
+        <Chat />
+      </div>
     </div>
   );
 }
---
+++ src/Sidebar.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Sidebar.css";
+
+const Sidebar = () => {
+  return (
+    <div className="sidebar">
+      <div className="sidebar__header">header</div>
+      <div className="sidebar__search">search</div>
+      <div className="sidebar__chats">chats</div>
+    </div>
+  );
+};
+
+export default Sidebar;
