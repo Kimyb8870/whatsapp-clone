6643732882d66a769260257088e8c95210e56ed9
kimyb8870 <qhd0084@gmail.com>
2020-12-21 02:52:46 +0900

    Add: Sidebar

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,12 +1,45 @@
 import React from "react";
 import "./Sidebar.css";
+import { Avatar, IconButton } from "@material-ui/core";
+import DonutLargeIcon from "@material-ui/icons/DonutLarge";
+import ChatIcon from "@material-ui/icons/Chat";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
+import SearchIcon from "@material-ui/icons/Search";
+import SidebarChat from "./SidebarChat";
 
 const Sidebar = () => {
   return (
     <div className="sidebar">
-      <div className="sidebar__header">header</div>
-      <div className="sidebar__search">search</div>
-      <div className="sidebar__chats">chats</div>
+      <div className="sidebar__header">
+        <Avatar />
+        <div className="sidebar__headerRight">
+          <IconButton>
+            <DonutLargeIcon />
+          </IconButton>
+          <IconButton>
+            <ChatIcon />
+          </IconButton>
+          <IconButton>
+            <MoreVertIcon />
+          </IconButton>
+        </div>
+      </div>
+
+      <div className="sidebar__search">
+        <div className="sidebar__searchContainer">
+          <SearchIcon />
+          <input placeholder="Search or start new chat" type="text" />
+        </div>
+      </div>
+
+      <div className="sidebar__chats">
+        <SidebarChat addNewChat />
+        <SidebarChat />
+        <SidebarChat />
+        <SidebarChat />
+        <SidebarChat />
+        <SidebarChat />
+      </div>
     </div>
   );
 };
